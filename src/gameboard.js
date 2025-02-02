d0978cf2e260434f67f07602ea72e1569f9d7ce3
codedstrings <mridhulml@gmail.com>
2025-02-02 19:15:24 +0530

    fix(gameboard): half ships are placed in half valid locations.

--- src/gameboard.js
+++ src/gameboard.js
@@ -5,29 +5,47 @@ class GameBoard{
         this.allShips = [];
     }
     placeShip(ship, x, y, isVertical){
-        if(isVertical){
-            for(let i=0; i<ship.length; i++){
-                if(this.#isPlacementValid(x+i, y)) {
-                    this.board[x+i][y] = ship;
-                } else {
-                    return false;
-                }
-            }
-        }else{
-            for(let i=0; i<ship.length; i++){
-                if(this.#isPlacementValid(x, y+i)) {
-                    this.board[x][y+i] = ship;
-                } else {
-                    return false;
-                }
-            }
+        if(!this.#isPlacementValid(x, y, ship.length, isVertical)){
+            return false;
         }
+        for(let i=0; i<ship.length; i++){
+            isVertical ? 
+                this.board[x+i][y] = ship:
+                this.board[x][y+i] = ship;
+        }
+
         this.allShips.push(ship);
         return true;
     }
 
-    #isPlacementValid(x, y) {
-        return x >= 0 && x < 10 && y >= 0 && y < 10 && this.board[x][y] === null;
+    #isPlacementValid(x, y, length, isVertical) {
+        // Check if ship is out of bounds
+        if(isVertical){
+            if(x + length > 10){
+                return false;
+            }
+        }else{
+            if(y + length > 10){
+                return false;
+            }
+        }
+
+        //check if ship's space is already occupied
+        for(let i=0; i<length; i++){
+            if(isVertical){
+                if(this.board[x+i][y] !== null){
+                    return false;
+                }
+            }else{
+                if(this.board[x][y+i] !== null){
+                    return false;
+                }
+            }
+        }
+        
+        //todo : check if any adjacent spaces are occupied
+
+        return true;
     }
 
     receiveAttack(x, y){
@@ -42,5 +60,6 @@ class GameBoard{
     allSunk(){
         return this.allShips.every(ship => ship.isSunk);
     }
+    
 }
 exports.GameBoard = GameBoard;
